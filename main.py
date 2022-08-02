658d2dcdcdb7032bf5489c38660ed1defcafc924
Said Salas <omarsaidbrito@gmail.com>
2022-08-01 21:42:32 -0500

    Creation of root, books and book

--- main.py
+++ main.py
@@ -1,8 +1,42 @@
-class __variables():
-    pass
+from fastapi import FastAPI
+import sqlite3
 
-def run():
-    pass
+def kjdb():
 
-if __name__="__main__":
-    run()
+    connect = sqlite3.connect('/home/supremez41d/Documents/Python_Folder/LionsRoar/KJ_Bible')
+    cursor = connect.cursor()
+
+    return cursor
+
+
+
+app = FastAPI()
+    
+@app.get('/')
+def root():
+    return {'message': 'Welcome to the King James RestAPI Bible!'}
+
+@app.get('/books/')
+def books():
+
+    cursor = kjdb()
+
+    command = """SELECT name
+    FROM sqlite_schema
+    WHERE type='table' AND name NOT LIKE 'sqlite_%'"""
+    
+    book_list = cursor.execute(command).fetchall()
+    
+    return {'books': book_list}
+
+@app.get('/books/{book}')
+def book(book):
+    cursor = kjdb()
+
+    command = """SELECT *
+    FROM Genesis"""
+
+    verse_list = cursor.execute(command).fetchall()
+
+    return {'book': f'{book}',
+            'verses': verse_list}
