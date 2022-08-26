bbd63634826f9835494765e0c6eed10d4cd9fbdf
SupremeZ41D <54646072+supremeZ41D@users.noreply.github.com>
2022-08-25 22:20:50 -0500

    Update main.py
    - Create a welcome text.
    - List the books of the Bible in a column and row form.

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 import typer
+import sqlite3
 
+# Initial variables
+connect = sqlite3.connect("KJ_Bible")
+cursor = connect.cursor()
+app = typer.Typer()
 
-def main(name: str):
-    print(f"Hello {name}")
+@app.command()
+def welcome(name: str):
+    print(f"Welcome {name}, to Roar! An open-source CLI Bible tool.")
+
+@app.command()
+def books(mode: str='column'):
+    query = """SELECT name FROM sqlite_schema"""
+    result = cursor.execute(query).fetchall()
+
+    for cell in result:
+        if mode=='row':
+            print(cell[0], end=' ')
+        elif mode=='column':
+            print(cell[0])
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
