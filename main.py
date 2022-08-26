b160caf28fa47ac1dc10494d917797d3eca79b62
SupremeZ41D <54646072+supremeZ41D@users.noreply.github.com>
2022-08-25 21:55:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,9 @@
-from fastapi import FastAPI
-import sqlite3
-
-def kjdb():
-
-    connect = sqlite3.connect('/home/supremez41d/Documents/Python_Folder/LionsRoar/KJ_Bible')
-    cursor = connect.cursor()
-
-    return cursor
+import typer
 
 
+def main(name: str):
+    print(f"Hello {name}")
 
-app = FastAPI()
-    
-@app.get('/')
-def root():
-    return {'message': 'Welcome to the King James RestAPI Bible!'}
 
-@app.get('/books/')
-def books():
-
-    cursor = kjdb()
-
-    command = """SELECT name
-    FROM sqlite_schema
-    WHERE type='table' AND name NOT LIKE 'sqlite_%'"""
-    
-    book_list = cursor.execute(command).fetchall()
-    
-    return {'books': book_list}
-
-@app.get('/books/{book}')
-def book(book):
-    cursor = kjdb()
-
-    command = """SELECT *
-    FROM Genesis"""
-
-    verse_list = cursor.execute(command).fetchall()
-
-    return {'book': f'{book}',
-            'verses': verse_list}
+if __name__ == "__main__":
+    typer.run(main)
