ef4fb3edba80184a77a1802a9f5b5bacf57f9849
SupremeZ41D <54646072+supremeZ41D@users.noreply.github.com>
2022-09-06 12:20:33 -0500

    Changes in Author DB
    - Try to adjust how the authors and books are displayed.
    - Complete the authors for the books.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import typer
 import sqlite3
+import pandas as pd
 
 # Initial variables
-connect = sqlite3.connect("KJ_Bible")
-cursor = connect.cursor()
+connect_kj = sqlite3.connect("KJ_Bible")
+cursor_kj = connect_kj.cursor()
+connect_auth = sqlite3.connect('Authors_KJB.db')
+cursor_auth = connect_auth.cursor()
 app = typer.Typer()
 
 @app.command()
@@ -14,15 +17,29 @@ def welcome(name: str):
 def books(mode: str='column', author: bool=False):
     """Books are printed in both column and row form"""
 
-    query = """SELECT name FROM sqlite_schema"""
-    result = cursor.execute(query).fetchall()
+    def authors(place_list):
+        df = pd.DataFrame(place_list, columns=['Book','Author'])
+        print(df.groupby(['Author'])['Book'].value_counts().to_frame().rename(columns={'Book':'count'}).reset_index(level='Book')['Book'])
 
-    for cell in result:
-        if mode=='row':
+    query_kj = """SELECT name FROM sqlite_schema"""
+    result_kj = cursor_kj.execute(query_kj).fetchall()
+    query_auth = """SELECT Book,Author FROM Book2Author"""
+    result_auth = cursor_auth.execute(query_auth).fetchall()
+
+    place_list = []
+
+    if mode=='row':
+        for cell in result_kj:
             print(cell[0], end=' ')
-        elif mode=='column':
+    elif mode=='column' and author is False:
+        for cell in result_kj:
             print(cell[0])
+    elif mode=='column' and author is True:
+        for cell in result_auth:
+            #place_list.append(cell)
+            print(cell[0] + ': ' + cell[1])
 
+        #authors(place_list)
 
 
 
